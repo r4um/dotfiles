19f0d618fe97c561a8aa9a2b6481914dba59954a
Pranay Kanwar <pranay.kanwar@gmail.com>
2019-05-08 21:56:51 +0530

    update editor configs, drop .gitmodules

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -1,30 +1,79 @@
 (setq-default dotspacemacs-themes '(zenburn))
 (setq dotspacemacs-additional-packages '(gradle-mode))
-(setq-default dotspacemacs-configuration-layers '(
-      csv
-      auto-completion
-      (c-c++ :variables c-c++-enable-clang-support t c-c++-default-mode-for-headers 'c++-mode)
-      clojure
-      docker
-      elixir
-      emacs-lisp
-      ess
-      git
-      go
-      haskell
-      html
-      (java :variables java-backend 'meghanda)
-      javascript
-      groovy
-      ipython-notebook
-      latex
-      lua
-      markdown
-      python
-      ruby
-      rust
-      scala
-      shell
-      shell-scripts
-      yaml
-))
+(setq-default
+ dotspacemacs-configuration-layers
+ '(rust
+   agda
+   asm
+   autohotkey
+   bibtex
+   (c-c++ :variables c-c++-enable-clang-support t c-c++-default-mode-for-headers 'c++-mode)
+   clojure
+   coffeescript
+   common-lisp
+   coq
+   crystal
+   csharp
+   csv
+   d
+   dart
+   dhall
+   elixir
+   elm
+   emacs-lisp
+   erlang
+   ess
+   factor
+   faust
+   forth
+   fsharp
+   go
+   gpu
+   graphviz
+   groovy
+   haskell
+   html
+   hy
+   idris
+   ipython-notebook
+   (java :variables java-backend 'meghanda)
+   javascript
+   jr
+   json
+   jsonnet
+   julia
+   kotlin
+   latex
+   lua
+   major-modes
+   markdown
+   nim
+   ocaml
+   octave
+   pact
+   perl5
+   perl6
+   php
+   plantuml
+   prolog
+   protobuf
+   purescript
+   python
+   racket
+   restructuredtext
+   ruby
+   rust
+   scala
+   scheme
+   semantic-web
+   shell-scripts
+   sml
+   sql
+   swift
+   typescript
+   vimscript
+   windows-scripts
+   yaml
+   yang
+   )
+ )
